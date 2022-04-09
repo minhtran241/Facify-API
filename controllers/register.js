08e210046d0be861e43ad48b935f8fae3fba0eba
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-04-09 19:33:36 -0400

    First commit

---
+++ controllers/register.js
@@ -0,0 +1,36 @@
+const handleRegister = (req, res, db, bcrypt) => {
+	const { email, password, firstName, lastName } = req.body;
+	if (!email || !password || !firstName || !lastName) {
+		return res.status(400).json("incorrect form submission");
+	}
+	const hash = bcrypt.hashSync(password, 10);
+	// Store hash in your password DB.
+	db.transaction((trx) => {
+		trx
+			.insert({
+				hash: hash,
+				email: email,
+			})
+			.into("login")
+			.returning("email")
+			.then((loginEmail) => {
+				return trx("users")
+					.returning("*")
+					.insert({
+						email: loginEmail[0].email,
+						firstname: firstName,
+						lastname: lastName,
+						joined: new Date(),
+					})
+					.then((user) => {
+						res.json(user[0]);
+					});
+			})
+			.then(trx.commit)
+			.catch(trx.rollback);
+	}).catch((err) => res.status(400).json("unable to register"));
+};
+
+module.exports = {
+	handleRegister: handleRegister,
+};
